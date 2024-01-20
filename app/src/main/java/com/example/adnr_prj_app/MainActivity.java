3370b23e9dd02e89140a647494ec991de0807c2a
realayna <100434027+realayna@users.noreply.github.com>
2024-01-20 18:26:16 +0100

    added buttons and fields

--- app/src/main/java/com/example/adnr_prj_app/MainActivity.java
+++ app/src/main/java/com/example/adnr_prj_app/MainActivity.java
@@ -1,8 +1,18 @@
 package com.example.adnr_prj_app;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
 
